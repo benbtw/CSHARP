a11d7af219fd0e8ae693a218e1aac7d335005ce6
benbtw <58542595+benbtw@users.noreply.github.com>
2023-11-22 12:16:18 -0600

    ...

--- TopDown2D/TopDown2D/src/Player.cs
+++ TopDown2D/TopDown2D/src/Player.cs
@@ -7,21 +7,62 @@ namespace TopDown2D
     internal class Player
     {
         Texture2D player;
-        Texture2D playerWalk;
-        Texture2D playerIdle;
-        Texture2D[] playerIdleAnim;
-        Texture2D[] playerWalkAnim;
+        Rectangle[] playerIdleAnim;
+        Rectangle[] playerUpAnim;
+        Rectangle[] playerDownAnim;
+        Rectangle[] playerLeftAnim;
+        Rectangle[] playerRightAnim;
         private Vector2 position;
         private float movementSpeed;
+        private Rectangle spriteSelector;
+        bool facing_right, facing_up, facing_down, facing_left;
+        int index = 0;
+        int delay = 0;
 
         public Player()
         {
             position = new Vector2(10, 10);
-            movementSpeed = 5f;
+            spriteSelector = new Rectangle(0, 0, 16, 16);
+            movementSpeed = 4f;
+            
+            //player animations
+            playerUpAnim = new Rectangle[4];
+            playerUpAnim[0] = new Rectangle(16, 0, 16, 16);
+            playerUpAnim[1] = new Rectangle(16, 16, 16, 16);
+            playerUpAnim[2] = new Rectangle(16, 32, 16, 16);
+            playerUpAnim[3] = new Rectangle(16, 48, 16, 16);
+
+
+            playerDownAnim = new Rectangle[4];
+            playerDownAnim[0] = new Rectangle(0, 0, 16, 16);
+            playerDownAnim[1] = new Rectangle(0, 16, 16, 16);
+            playerDownAnim[2] = new Rectangle(0, 32, 16, 16);
+            playerDownAnim[3] = new Rectangle(0, 48, 16, 16);
+
+
+            playerLeftAnim = new Rectangle[4];
+            playerLeftAnim[0] = new Rectangle(32, 0, 16, 16);
+            playerLeftAnim[1] = new Rectangle(32, 16, 16, 16);
+            playerLeftAnim[2] = new Rectangle(32, 32, 16, 16);
+            playerLeftAnim[3] = new Rectangle(32, 48, 16, 16);
+
+
+            playerRightAnim = new Rectangle[4];
+            playerRightAnim[0] = new Rectangle(48, 0, 16, 16);
+            playerRightAnim[1] = new Rectangle(48, 16, 16, 16);
+            playerRightAnim[2] = new Rectangle(48, 32, 16, 16);
+            playerRightAnim[3] = new Rectangle(48, 48, 16, 16);
+
+            playerIdleAnim = new Rectangle[4];
+            playerIdleAnim[0] = new Rectangle(0, 0, 16, 16); // down
+            playerIdleAnim[1] = new Rectangle(16, 0, 16, 16); // up
+            playerIdleAnim[2] = new Rectangle(32, 0, 16, 16); // left
+            playerIdleAnim[3] = new Rectangle(48, 0, 16, 16); // right
         }
 
         private void HandleInput(KeyboardState kbState)
         {
+            delay++;
             bool upKeyPressed = kbState.IsKeyDown(Keys.W);
             bool downKeyPressed = kbState.IsKeyDown(Keys.S);
             bool leftKeyPressed = kbState.IsKeyDown(Keys.A);
@@ -30,27 +71,73 @@ namespace TopDown2D
             if (upKeyPressed)
             {
                 position.Y -= movementSpeed;
+                facing_up = true;
+                facing_down = false;
+                facing_left = false;
+                facing_right = false;
+                spriteSelector = playerUpAnim[index];
             }
-            if (downKeyPressed)
+            else if (downKeyPressed)
             {
                 position.Y += movementSpeed;
+                facing_down = true;
+                facing_up = false;
+                facing_left = false;
+                facing_right = false;
+                spriteSelector = playerDownAnim[index];
             }
-            if (leftKeyPressed)
+           else if (leftKeyPressed)
             {
                 position.X -= movementSpeed;
+                facing_left = true;
+                facing_right = false;
+                facing_down = false;
+                facing_up = false;
+                spriteSelector = playerLeftAnim[index];
             }
-            if (rightKeyPressed)
+            else if (rightKeyPressed)
             {
                 position.X += movementSpeed;
+                facing_right = true;
+                facing_left = false;
+                facing_down = false;
+                facing_up = false;
+                spriteSelector = playerRightAnim[index];
+            }
+
+            if (delay > 10)
+            {
+                delay = 0;
+                index++;
+                if (index == 4)
+                    index = 0;
+            }
+
+            if (kbState.IsKeyUp(Keys.W) && kbState.IsKeyUp(Keys.S) && 
+                kbState.IsKeyUp(Keys.A) && kbState.IsKeyUp(Keys.D))
+            {
+                if (facing_up)
+                {
+                    spriteSelector = playerIdleAnim[1];
+                }
+                if (facing_down)
+                {
+                    spriteSelector = playerIdleAnim[0];
+                }
+                if (facing_left)
+                {
+                    spriteSelector = playerIdleAnim[2];
+                }
+                if (facing_right)
+                {
+                    spriteSelector = playerIdleAnim[3];
+                }
             }
         }
 
         public void loadContent(Game1 main)
         {
-            player = main.Content.Load<Texture2D>("Idle");
-            playerIdle = main.Content.Load<Texture2D>("Idle");
-            playerWalk = main.Content.Load<Texture2D>("Walk");
-
+            player = main.Content.Load<Texture2D>("SpriteSheet");
         }
 
         public void Update(GameTime gameTime)
@@ -61,7 +148,7 @@ namespace TopDown2D
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(player, position, new Rectangle(0,0,16,16), Color.White);
+            spriteBatch.Draw(player, position, spriteSelector, Color.White);
         }
     }
 }
